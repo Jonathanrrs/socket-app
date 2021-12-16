c6d9358526f0b07d402709ca861c29eea989d326
jonathan <yoni_12rrs@hotmail.com>
2021-12-15 21:23:37 -0600

    Mensajes de conexión y desconexión - Cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,2 +1,20 @@
+/* Referencias del html */
+
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 
 const socket = io();
+
+socket.on('connect', () => {
+    console.log('conectado');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    console.log('desconectado');
+
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+});
